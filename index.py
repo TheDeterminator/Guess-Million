b2810188aef03460a13167a95f58d95077868113
Alec <afroenygam@gmail.com>
2018-12-06 12:51:15 -0500

    basic REPL  updated

--- index.py
+++ index.py
@@ -1,3 +1,12 @@
+import random
 
-
+while(True):
+    user_input = input('Enter command: ')
+    if user_input == 'r':
+        print(random.randint(1,4))
+    elif user_input == 'q':
+        print('quit selected')
+        break
+    else:
+        print('invalid command')
 
